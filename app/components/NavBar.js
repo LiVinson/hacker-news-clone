1eca65cd7ecd75b1d071c84756336fa46fbae04d
Lisa Vinson <Lrv0828@gmail.com>
2020-04-03 16:59:52 -0400

    added theme context. Added changeTheme function in app state, passing to navbar

--- app/components/NavBar.js
+++ app/components/NavBar.js
@@ -1,28 +1,41 @@
 import React from "react"
 import { NavLink } from "react-router-dom"
+import { ThemeConsumer } from "../context/theme"
 
 const styles = {
   color: "rgb(204, 20, 20)",
   fontWeight: "bold"
 }
+
 export default function NavBar() {
   return (
-    <nav className="navbar row space-around">
-      <ul className="row">
-        <li className="list-item">
-          <NavLink exact to="/" activeStyle={styles}>
-            Top
-          </NavLink>
-        </li>
-        <li className="list-item">
-          <NavLink to="/new" activeStyle={styles}>
-            New
-          </NavLink>
-        </li>
-      </ul>
-      <button style={{ fontSize: "30px" }} className="btn-clear">
-        🌞
-      </button>
-    </nav>
+    <ThemeConsumer>
+      {({ theme, changeTheme }) => {
+        return (
+          <nav className="navbar row space-around">
+            <ul className="row">
+              <li className="list-item">
+                <NavLink exact to="/" activeStyle={styles}>
+                  Top
+                </NavLink>
+              </li>
+              <li className="list-item">
+                <NavLink to="/new" activeStyle={styles}>
+                  New
+                </NavLink>
+              </li>
+            </ul>
+
+            <button
+              style={{ fontSize: "30px" }}
+              className="btn-clear"
+              onClick={changeTheme}
+            >
+              {theme === "light" ? "🌙" : "🌞"}
+            </button>
+          </nav>
+        )
+      }}
+    </ThemeConsumer>
   )
 }
--- app/index.js
+++ app/index.js
@@ -7,27 +7,48 @@ import User from "./components/User"
 import Comment from "./components/Comment"
 import { Container } from "./components/Container"
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
-
+import { ThemeProvider } from "./context/theme"
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      theme: "light",
+      changeTheme: () => {
+        console.log("theme change")
+        this.setState(curState => {
+          return {
+            theme: curState.theme === "light" ? "dark" : "light"
+          }
+        })
+      }
+    }
+
+    this.changeTheme = this.changeTheme.bind(this)
+  }
+
+  changeTheme() {}
+
   render() {
     return (
       <Router>
         <Container>
-          <Navbar />
-          <Switch>
-            <Route
-              exact
-              path="/"
-              render={props => <News {...props} storyType="top" />}
-            />
-            <Route
-              exact
-              path="/new"
-              render={props => <News {...props} storyType="new" />}
-            />
-            <Route path="/user" component={User} />
-            <Route path="/post" component={Comment} />
-          </Switch>
+          <ThemeProvider value={this.state}>
+            <Navbar />
+            <Switch>
+              <Route
+                exact
+                path="/"
+                render={props => <News {...props} storyType="top" />}
+              />
+              <Route
+                exact
+                path="/new"
+                render={props => <News {...props} storyType="new" />}
+              />
+              <Route path="/user" component={User} />
+              <Route path="/post" component={Comment} />
+            </Switch>
+          </ThemeProvider>
         </Container>
       </Router>
     )
@@ -36,20 +57,11 @@ class App extends React.Component {
 
 //Next Steps:
 
-//Style the News component
-//User component
-//API: get posts by the user that was selected. Reuse Card component
-//Get it working with loading
-
-//Style User component
-//Comment Components -
-//API: get comments based on postId.
-//may be able to resuse card with props.children. if not, can atleast reuse date formatting function
-//includes links to commenters page
-
-//Remaining styling
-//Add in routing
-//Implement Dark theme
-//Testing?
+//Minor styling tweaks - navbar top,
+//Implement Dark theme with context, add state to App to manage
+//Clean up API
+//Clean up commenting
+//Readme
+//Production Ready
 
 ReactDom.render(<App />, document.getElementById("root"))
